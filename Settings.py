3e45a4deb40c573f1ead1531e963b049ff35cd38
9phowell <9phowell@gunston.org>
2017-02-01 13:17:53 -0500

    respawns correctly now
    Signed-off-by: 9phowell <9phowell@gunston.org>

--- Settings.py
+++ Settings.py
@@ -13,7 +13,7 @@ class Settings():
         # Initialize the game's settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (210, 240, 255)
+        self.bg_color = (0, 0, 0)
 
         # Ship Settings
         self.ship_speed_factor = 1.5
