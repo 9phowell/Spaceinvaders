972c0ff0a2f10d5c796e846e4d15028d1683d93d
9phowell <9phowell@gunston.org>
2017-02-06 08:55:37 -0500

    This version displays a display button
    a1.14.2

--- Gamestats.py
+++ Gamestats.py
@@ -2,17 +2,21 @@
 class GameStats():
     """Track statistics for Alien Invasion."""
 
-    if __name__ == '__main__':
-        def __init__(self, ai_settings):
-            """Initialize statistics."""
-            self.ai_settings = ai_settings
-            self.reset_stats()
-            # Start alien invasion in an active stats
-            self.game_active = True
+    def __init__(self, ai_settings):
+        """Initialize statistics."""
+        self.ai_settings = ai_settings
+        self.reset_stats()
 
-            # Start game in an inactive state
-            self.game_active = False
+        # Start game in an inactive state.
+        self.game_active = False
+
+        # High score should never be reset. For Future Use
+        self.high_score = 0
 
     def reset_stats(self):
         """Initialize statistics that can change during the game."""
         self.ships_left = self.ai_settings.ship_limit
+
+        # For Future Use
+        self.score = 0
+        self.level = 1
